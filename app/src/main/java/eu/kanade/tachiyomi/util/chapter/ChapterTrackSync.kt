b3c93cacd0d2a1bb1b4161e2db050db3d9545ad8
TeamDman <TeamDman9201@gmail.com>
2021-08-05 17:12:31 -0400

    Update logic to ensure chapter numbers are used rather than indices

--- app/src/main/java/eu/kanade/tachiyomi/util/chapter/ChapterTrackSync.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/chapter/ChapterTrackSync.kt
@@ -6,6 +6,7 @@ import eu.kanade.tachiyomi.data.database.models.Track
 import eu.kanade.tachiyomi.data.track.TrackService
 import eu.kanade.tachiyomi.util.lang.launchIO
 import timber.log.Timber
+import kotlin.math.max
 
 /**
  * Helper method for syncing a remote track with the local chapters, and back
@@ -16,21 +17,25 @@ import timber.log.Timber
  * @param service the tracker service.
  */
 fun syncChaptersWithTrackServiceTwoWay(db: DatabaseHelper, chapters: List<Chapter>, remoteTrack: Track, service: TrackService) {
-    val sortedChapters = chapters.sortedBy { it.chapter_number }
-    sortedChapters
-        .filterIndexed { index, chapter -> index < remoteTrack.last_chapter_read && !chapter.read }
-        .forEach { it.read = true }
-    db.updateChaptersProgress(sortedChapters).executeAsBlocking()
+    // mark local chapters read based on remote
+    chapters
+            .filter { !it.read }
+            .filter { it.chapter_number < remoteTrack.last_chapter_read }
+            .forEach{ it.read = true }
+    db.updateChaptersProgress(chapters).executeAsBlocking()
 
-    val localLastRead = when {
-        sortedChapters.all { it.read } -> sortedChapters.size
-        sortedChapters.any { !it.read } -> sortedChapters.indexOfFirst { !it.read }
-        else -> 0
-    }
+    // find last read local chapter number
+    // default to existing tracker value
+    val lastReadLocalChapterNumber = chapters
+            .filter { it.read }
+            .maxByOrNull { it.chapter_number }
+            ?.chapter_number?.toInt()
+            ?:remoteTrack.last_chapter_read
 
-    // update remote
-    remoteTrack.last_chapter_read = localLastRead
+    // update tracker, no backtracking allowed
+    remoteTrack.last_chapter_read = max(remoteTrack.last_chapter_read, lastReadLocalChapterNumber)
 
+    // publish changes
     launchIO {
         try {
             service.update(remoteTrack)
